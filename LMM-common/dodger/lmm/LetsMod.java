39a7d6b55b66e42ff710b39ad0c848b1cb4b4734
jdodger415 <jdodger415@gmail.com>
2013-07-06 12:18:52 +0100

    added first parts to the/my mod

---
+++ LMM-common/dodger/lmm/LetsMod.java
@@ -0,0 +1,29 @@
+package dodger.lmm;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import dodger.lmm.lib.Referance;
+
+@Mod(modid = Referance.MOD_ID, name = Referance.MOD_NAME, version = Referance.VERSION)
+public class LetsMod {
+
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event) {
+        
+    }
+    
+    @Init
+    public void Init(FMLInitializationEvent event) {
+        
+    }
+    
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event) {
+        
+    }
+}
---
+++ LMM-common/dodger/lmm/lib/Referance.java
@@ -0,0 +1,9 @@
+package dodger.lmm.lib;
+
+public class Referance {
+    
+    public static final String MOD_ID = "LMM";
+    public static final String MOD_NAME = "Let's Mod Mod";
+    public static final String VERSION = "0.1";
+
+}
